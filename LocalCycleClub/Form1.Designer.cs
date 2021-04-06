dbbccc196daf6c84c9c9f0b4fd71ee6ff617bbee
m-a-z-z-z <dylanmaz@protonmail.com>
2021-04-06 10:38:57 +0100

    Yurt

--- LocalCycleClub/Form1.Designer.cs
+++ LocalCycleClub/Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace LocalCycleClub
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabActivities = new System.Windows.Forms.TabPage();
+            this.Leaderboard = new System.Windows.Forms.TabPage();
+            this.tabProfile = new System.Windows.Forms.TabPage();
+            this.tabSignup = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabHome);
+            this.tabControl1.Controls.Add(this.tabActivities);
+            this.tabControl1.Controls.Add(this.Leaderboard);
+            this.tabControl1.Controls.Add(this.tabProfile);
+            this.tabControl1.Controls.Add(this.tabSignup);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 528);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabHome
+            // 
+            this.tabHome.Location = new System.Drawing.Point(4, 25);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(768, 499);
+            this.tabHome.TabIndex = 0;
+            this.tabHome.Text = "Home";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // tabActivities
+            // 
+            this.tabActivities.Location = new System.Drawing.Point(4, 25);
+            this.tabActivities.Name = "tabActivities";
+            this.tabActivities.Padding = new System.Windows.Forms.Padding(3);
+            this.tabActivities.Size = new System.Drawing.Size(768, 499);
+            this.tabActivities.TabIndex = 1;
+            this.tabActivities.Text = "Activities";
+            this.tabActivities.UseVisualStyleBackColor = true;
+            // 
+            // Leaderboard
+            // 
+            this.Leaderboard.Location = new System.Drawing.Point(4, 25);
+            this.Leaderboard.Name = "Leaderboard";
+            this.Leaderboard.Size = new System.Drawing.Size(768, 499);
+            this.Leaderboard.TabIndex = 2;
+            this.Leaderboard.Text = "tabLeaderboard";
+            this.Leaderboard.UseVisualStyleBackColor = true;
+            // 
+            // tabProfile
+            // 
+            this.tabProfile.Location = new System.Drawing.Point(4, 25);
+            this.tabProfile.Name = "tabProfile";
+            this.tabProfile.Size = new System.Drawing.Size(768, 499);
+            this.tabProfile.TabIndex = 3;
+            this.tabProfile.Text = "Profile";
+            this.tabProfile.UseVisualStyleBackColor = true;
+            // 
+            // tabSignup
+            // 
+            this.tabSignup.Location = new System.Drawing.Point(4, 25);
+            this.tabSignup.Name = "tabSignup";
+            this.tabSignup.Size = new System.Drawing.Size(768, 499);
+            this.tabSignup.TabIndex = 4;
+            this.tabSignup.Text = "Sign Up";
+            this.tabSignup.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabActivities;
+        private System.Windows.Forms.TabPage Leaderboard;
+        private System.Windows.Forms.TabPage tabProfile;
+        private System.Windows.Forms.TabPage tabSignup;
     }
 }
 
--- LocalCycleClub/Form1.cs
+++ LocalCycleClub/Form1.cs
@@ -16,5 +16,10 @@ namespace LocalCycleClub
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
