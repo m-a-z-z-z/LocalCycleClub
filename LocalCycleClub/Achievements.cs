5b40ca6164c308f20afbf5cd512d87fdf9846ef4
m-a-z-z-z <dylanmaz@protonmail.com>
2021-04-18 14:15:00 +0100

    Added individual classes instead of one class file for all of them

---
+++ LocalCycleClub/Achievements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalCycleClub
+{
+    class Achievements
+    {
+        List<string> Milestones { get; set; }
+        DateTime DateAcheived { get; set; }
+        int NumOfMilestone { get; set; }
+        string MilestoneDescription { get; set; }
+        List<string> AchievableMilestone { get; set; }
+        int NumOfTimesEarned { get; set; }
+        string MotivationSentence { get; set; }
+        DateTime DateStarted { get; set; }
+
+        public Achievements(List<string> milestone, DateTime dateAchieved, int numMilestones, string milestoneDesc, List<string> achievableMs, int numTimesEarned, string motivation, DateTime dateStarted)
+        {
+            this.Milestones = new List<string>();
+            this.DateAcheived = dateAchieved;
+            this.NumOfMilestone = numMilestones;
+            this.MilestoneDescription = milestoneDesc;
+            this.AchievableMilestone = new List<string>();
+            this.NumOfTimesEarned = numTimesEarned;
+            this.MotivationSentence = motivation;
+            this.DateAcheived = dateStarted;
+        }
+    }
+}
---
+++ LocalCycleClub/Activities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalCycleClub
+{
+    class Activities
+    {
+        string ActivityName { get; set; }
+        string Location { get; set; }
+        DateTime Date { get; set; }
+        string ActivityDescription { get; set; }
+        List<string> ResourcesRequired { get; set; }
+        public string BadgeEarned { get; set; }
+
+        public Activities(string activityName,string location, DateTime date, string activityDesc, List<string> resourcesReq, string badgeEarned)
+        {
+            this.ActivityName = activityName;
+            this.Location = location;
+            this.Date = date;
+            this.ActivityDescription = activityDesc;
+            this.ResourcesRequired = resourcesReq;
+            this.BadgeEarned = badgeEarned;
+        }
+    }
+}
